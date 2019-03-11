fb11d32b2e191d9ebc065edefc831d8f6e02b360
coulia <julien.regnier111@gmail.com>
2019-03-11 15:34:19 +0100

    aez

--- tests/MultiplyTest.php
+++ tests/MultiplyTest.php
@@ -25,4 +25,14 @@ class MultiplyTest extends TestCase
     {
         $this->assertSame(20, $this->multiply->multiplyBy5(4));
     }
+	
+	public function testC()
+    {
+        $this->assertSame(35, $this->multiply->multiplyBy7(5));
+    }
+
+    public function testD()
+    {
+        $this->assertSame(28, $this->multiply->multiplyBy7(4));
+    }
 }
